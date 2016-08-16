85404400e350597b84db1740aa022d9e890e9097
josemalyson <josemalyson@gmail.com>
2016-08-16 10:46:30 -0300

    Configuração de Layout

---
+++ aiport/src/main/resources/static/layout/js/usuario.js
@@ -0,0 +1,20 @@
+var btsalvar = $('#salvar');
+
+btsalvar.prop("disabled" , true);
+
+var senha = $('#senha');
+var confirmarSenha = $('#confirmarSenha');
+
+
+confirmarSenha.blur(function() {
+	if (senha.val() !== confirmarSenha.val()) {
+		senha.val(null);
+		confirmarSenha.val(null);
+		$('#modal').click();
+		btsalvar.prop("disabled" , true);
+		$('#conteudo').html("<p>Senha não confere com a digitada.</>");
+	}else{
+		btsalvar.prop("disabled" , false);
+	}
+		
+});
