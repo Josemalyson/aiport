2861b7b84af70e3bfbfb419ea53014d8a0630aea
josemalyson <josemalyson@gmail.com>
2016-08-19 00:29:22 -0300

    Corrigindo consulta para usuar Spring Data Jpa

--- aiport/src/main/java/com/aiport/repository/VooRepository.java
+++ aiport/src/main/java/com/aiport/repository/VooRepository.java
@@ -2,9 +2,7 @@ package com.aiport.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.aiport.entity.Voo;
@@ -12,7 +10,6 @@ import com.aiport.entity.Voo;
 @Repository
 public interface VooRepository extends CrudRepository<Voo, Long> {
 
-	  @Query(value="SELECT * FROM TB_JET_VOO WHERE fk_cidade_destino = :destino and fk_cidade_origem = :origem ", nativeQuery = true)
-	  public List<Voo> findByOrigemDestino(@Param("origem") Long origem, @Param("destino") Long destino);
-	
+	public List<Voo> findByOrigemIdAndDestinoId(Long origem, Long destino);
+
 }
